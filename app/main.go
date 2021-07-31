82734115b82d5db7fa38dcba5a88d06ba57b0b90
yzdann <22779039+yzdann@users.noreply.github.com>
2021-07-31 22:03:23 +0430

    Add totalReq, respStatus and httpDuration metrics

--- app/main.go
+++ app/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -11,20 +13,57 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{w, http.StatusOK}
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
 var (
+	totalRequests = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Number of get requests.",
+		},
+		[]string{"path"},
+	)
+
+	responseStatus = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "response_status",
+			Help: "Status of HTTP response",
+		},
+		[]string{"status"},
+	)
+
 	httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
-		Name: "jakub_app_http_duration_seconds",
+		Name: "http_response_time_seconds",
 		Help: "Duration of HTTP requests.",
 	}, []string{"path"})
 )
 
-// prometheusMiddleware implements mux.MiddlewareFunc.
 func prometheusMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		route := mux.CurrentRoute(r)
 		path, _ := route.GetPathTemplate()
+
 		timer := prometheus.NewTimer(httpDuration.WithLabelValues(path))
-		next.ServeHTTP(w, r)
+		rw := NewResponseWriter(w)
+		next.ServeHTTP(rw, r)
+
+		statusCode := rw.statusCode
+
+		responseStatus.WithLabelValues(strconv.Itoa(statusCode)).Inc()
+		totalRequests.WithLabelValues(path).Inc()
+
 		timer.ObserveDuration()
 	})
 }
@@ -33,12 +72,19 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home")
 }
 
+func init() {
+	prometheus.Register(totalRequests)
+	prometheus.Register(responseStatus)
+	prometheus.Register(httpDuration)
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.Use(prometheusMiddleware)
 	r.Path("/metrics").Handler(promhttp.Handler())
 	r.HandleFunc("/", HomeHandler)
 
+	log.Printf("Serving requests on port 8080")
 	srv := &http.Server{
 		Addr: "0.0.0.0:8080",
 		// Good practice to set timeouts to avoid Slowloris attacks.
@@ -47,5 +93,6 @@ func main() {
 		IdleTimeout:  time.Second * 60,
 		Handler:      r,
 	}
-	srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	log.Fatal(err)
 }
